a6dbb7721eb7887656c5832c2d3bbef04398366b
leonardo <leowalkg6@Gmail.com>
2025-07-20 21:04:19 -0300

    adicionando chamada  listar, atualizar ao controller

--- controllers/estoque_controller.py
+++ controllers/estoque_controller.py
@@ -23,3 +23,24 @@ class EstoqueController:
                         self.view.mostrar_mensagem("Erro ao adicionar produto, verifique se o produto já existe.")
                 except ValueError as e:
                     self.view.mostrar_mensagem(str(e))
+
+            elif opcao == '2':
+                produtos = self.model.listar_produtos()
+                self.view.mostrar_lista_produtos(produtos)
+
+            elif opcao == '3':
+                id = input('ID do produto a ser atualizado: ')
+                nome = input("Novo nome do produto (deixe em branco para não alterar): ")
+                preco = input("Novo preço do produto (deixe em branco para não alterar): ")
+                quantidade = input("Nova quantidade do produto (deixe em branco para não alterar): ")
+
+                preco = float(preco) if preco else None
+                quantidade = int(quantidade) if quantidade else None
+
+                try:
+                    if self.model.atualizar_produto(id, nome or None, preco, quantidade):
+                        self.view.mostrar_mensagem("Produto atualizado com sucesso.")
+                    else:
+                        self.view.mostrar_mensagem("Erro ao atualizar produto, verifique se o produto existe.")
+                except ValueError as e:
+                    self.view.mostrar_mensagem(str(e))
